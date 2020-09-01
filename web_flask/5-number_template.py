4941c07a977acf074538000cfb4e1318b7070e4c
karim bahri <karim-bahri-18@outlook.fr>
2020-09-01 21:50:37 +0000

    template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""display Hello HBNB / root
+   display HBNB /hbnb root
+   display /c/<text>
+   display /python/(<text>)
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def print_hello():
+    """return text to display using flask"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def print_hbnb():
+    """return text to display using flask"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def print_text(text):
+    """return text to display using flask"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def print_python(text):
+    """return text to display using flask"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def print_number(n):
+    """return text to display using flask"""
+    return "{:d} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def print_templates(n):
+    """render template"""
+    return render_template("5-number.html", number=n)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
